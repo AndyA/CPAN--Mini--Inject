add698567a4e083260299f6f15405ade0e646d71
Andy Armstrong <andy@hexten.net>
2009-06-15 14:19:41 +0100

    Bump version to 0.27

--- lib/CPAN/Mini/Inject/Config.pm
+++ lib/CPAN/Mini/Inject/Config.pm
@@ -12,11 +12,11 @@ CPAN::Mini::Inject::Config - Config for CPAN::Mini::Inject
 
 =head1 VERSION
 
-Version 0.26
+Version 0.27
 
 =cut
 
-our $VERSION = '0.26';
+our $VERSION = '0.27';
 
 =head2 C<new>
 
