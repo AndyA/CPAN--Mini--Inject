3fa7bdd36dc0ad5ac1f2fc65f7969d98308253b6
Christian Walde <walde.christian@googlemail.com>
2011-05-08 10:53:28 +0200

    bumped version numbers up because mcpani was already tagged with 0.28

--- lib/CPAN/Mini/Inject.pm
+++ lib/CPAN/Mini/Inject.pm
@@ -21,11 +21,11 @@ CPAN::Mini::Inject - Inject modules into a CPAN::Mini mirror.
 
 =head1 VERSION
 
-Version 0.27_02
+Version 0.28_01
 
 =cut
 
-our $VERSION = '0.27_02';
+our $VERSION = '0.28_01';
 our @ISA     = qw( CPAN::Mini );
 
 =head1 Synopsis
