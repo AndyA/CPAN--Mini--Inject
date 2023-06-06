eeb1010279bcc5f25f1d7a7c186637a4eaa6eb3e
Christian Walde <walde.christian@gmail.com>
2023-06-06 16:07:20 +0200

    released version 0.37

--- lib/CPAN/Mini/Inject.pm
+++ lib/CPAN/Mini/Inject.pm
@@ -22,11 +22,11 @@ CPAN::Mini::Inject - Inject modules into a CPAN::Mini mirror.
 
 =head1 VERSION
 
-Version 0.36
+Version 0.37
 
 =cut
 
-our $VERSION = '0.36';
+our $VERSION = '0.37';
 our @ISA     = qw( CPAN::Mini );
 
 =head1 Synopsis
--- lib/CPAN/Mini/Inject/Config.pm
+++ lib/CPAN/Mini/Inject/Config.pm
@@ -12,11 +12,11 @@ CPAN::Mini::Inject::Config - Config for CPAN::Mini::Inject
 
 =head1 VERSION
 
-Version 0.36
+Version 0.37
 
 =cut
 
-our $VERSION = '0.36';
+our $VERSION = '0.37';
 
 =head2 C<new>
 
